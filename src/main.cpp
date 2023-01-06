9faeb207bce4ba3862d726632bd19fa461b33ca2
Justin Godden <justingodden@gmail.com>
2023-01-06 15:47:39 +0000

    fix: temp string var define needed

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
 #include <string>
 #include <SFML/System/Time.hpp>
 #include "Chip8.h"
-#include "Disassembler.h"
 
 int main(int argc, char *argv[])
 {
     if (argc == 2)
     {
-        Chip8 chip8(std::string(argv[1]));
+        std::string fp(argv[1]);
+        Chip8 chip8(fp);
 
         return 0;
     }
