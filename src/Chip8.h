34aef0de05fc2de127dc3ceed9fddc46ebb324be
Justin Godden <justingodden@gmail.com>
2023-01-08 19:05:44 +0000

    chore: don't actually need a stack pointer

--- src/Chip8.h
+++ src/Chip8.h
@@ -36,7 +36,6 @@ private:
     std::array<uint8_t, 4096> memory;
     std::array<uint8_t, 64 * 32> display;
     std::stack<uint16_t> stack;
-    uint16_t sp;
     uint8_t delayTimer;
     uint8_t soundTimer;
     std::array<uint8_t, 16> keypad;
